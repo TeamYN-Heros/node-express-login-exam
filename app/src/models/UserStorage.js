beb3627fe5eeda45e733f29a05ff3bc1eb8a7c78
trofi92 <lunaslash9249@gmail.com>
2022-11-11 01:28:34 +0900

    login model, form

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -16,10 +16,10 @@ class UserStorage {
     // 4. hasOwnProperty를 통해 1에서 받은 값을 확인한다. 해당하는 배열
     //    (즉, id면 id가 key인 배열)을 호출하여 배열의 각 요소를 newUser에 누산시킨다.
     // 5. 이 과정을 반복하면 원본을 직접 노출시키지 않고
-    //    필요한 값을 새로운 배열(newUsers)로 반환하는 메서드를 만들 수 있다.
+    //    필요한 값을 새로운 객체(newUsers)로 반환하는 메서드를 만들 수 있다.
     // 6. newUsers의 return 값을 받은 컨트롤러 함수는
     //    그것이 제공한 parameter에 따라 값을 선별적으로 제공받아 사용하게 된다.
-    console.log(...fields);
+    // console.log(...fields);
     const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
@@ -28,6 +28,16 @@ class UserStorage {
       return newUsers;
     }, {}); // 빈 객체 = initialValue = newUsers 배열을 담는 역할
     return newUsers;
+  }
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users);
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    return userInfo;
   }
 }
 
